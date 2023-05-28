f34637844ae6e8a627c66d65bbbb85262e9048cb
Cipy348 <cipriangabriel112@gmail.com>
2023-05-28 21:37:16 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,77 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+class Id {
+private:
+    static int lastid;
+    int id;
+public:
+    Id() : id(lastid++) {}
+    const int getId() const {
+        return id;
+    }
+};
+template <typename T>
+int Id<T>::lastid = 0;
+
+class Person{
+private:
+    string name;
+    int age;
+public:
+    virtual void display(){
+        cout<<name<<" "<<age;
+    }
+    const void setName(const string name_){
+        name = name_;
+    }
+    const void setAge(const int age_){
+        age = age_;
+    }
+
+    const string getName(){
+        return name;
+    }
+    const int getAge(){
+        return age;
+    }
+
+    virtual ~Person();
+};
+
+class Fotbalist : public Person, public Id<Fotbalist>{
+private:
+    string pozitie;
+public:
+    void display(){
+        Person::display();
+        cout<<pozitie;
+    }
+    const void setPoz(const string pozitie){
+        this->pozitie = pozitie;
+    }
+    const string getPoz(){
+        return pozitie;
+    }
+    ~Fotbalist(){}
+};
+
+class Baschetbalist : public Person, public Id<Baschetbalist>{
+private:
+    int aruncaricos;
+public:
+    const void setAruncari(const int aruncaricos){
+        this->aruncaricos = aruncaricos;
+    }
+    const int getAruncari(){
+        return aruncaricos;
+    }
+    ~Baschetbalist(){}
+};
 
 int main() {
-    std::cout << "Hello, world!\n";
+
     return 0;
 }
